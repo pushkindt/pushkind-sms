8539a045d50591e64253bcaa3f3466c6f3d4b56d
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-11-07 14:13:34 +0300

    Minimal working prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use aws_config::BehaviorVersion;
+use aws_sdk_sns::{Client, types::MessageAttributeValue};
+use dotenvy::dotenv;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+    dotenv().ok(); // Load .env file
+
+    let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+
+    let client = Client::new(&config);
+
+    let sender_id = MessageAttributeValue::builder()
+        .data_type("String")
+        .string_value("cns.shared")
+        .build()
+        .unwrap();
+
+    let result = client
+        .publish()
+        .message_attributes("SenderID", sender_id)
+        .phone_number("+7xxxxxxxxxx")
+        .message("12345")
+        .send()
+        .await;
+    log::info!("{result:?}");
 }
